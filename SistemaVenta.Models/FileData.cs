ab949cd8ac1eafbe0ab3dca9355c9d4b27e8989e
prietoana321 <36928163+prietoana321@users.noreply.github.com>
2024-03-03 18:30:29 -0500

    AGREGUE LOS MODELOSc creados como clases comunes

---
+++ SistemaVenta.Models/FileData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SistemaVenta.Models
+{
+    public partial class FileData
+    {
+        public int IdFile { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Extension { get; set; }
+
+        public string? MimeType { get; set; }
+
+        public string? Path { get; set; }
+        public int? IdImagen { get; set; }
+
+        public virtual Imagen? IdImagenNavigation { get; set; }
+    }
+}
---
+++ SistemaVenta.Models/Imagen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVenta.Models
+{
+    public partial class Imagen
+    {
+        public int IdImagen { get; set; }
+
+        public string? Nombre { get; set; }
+
+        public virtual ICollection<FileData> FileData { get; } = new List<FileData>();
+    }
+}
